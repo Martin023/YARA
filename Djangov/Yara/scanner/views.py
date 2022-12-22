697b9ff2cd392b49f68c5d2cdb80e6e41f3be332
Martin023 <mainamartin023#gmail.com>
2022-12-22 17:27:06 +0300

    Upload to remote repo

--- Djangov/Yara/scanner/views.py
+++ Djangov/Yara/scanner/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import request
-from django import forms
+from .forms import UploadFileForm
+from django.http import HttpResponseRedirect
+from django.core.files.storage import FileSystemStorage
 
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            myfile = request.FILES['file']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(filename)
+            return render(request, 'upload.html', {
+                'form': form,
+                'uploaded_file_url': uploaded_file_url
+            })
+    else:
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form': form})
