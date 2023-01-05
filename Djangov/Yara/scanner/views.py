21e5f4942b674058106299b328ca5af903933bbe
Martin023 <mainamartin023@gmail.com>
2023-01-05 11:08:12 +0300

    Upload files functionality

--- Djangov/Yara/scanner/views.py
+++ Djangov/Yara/scanner/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.http import request
 from .forms import UploadFileForm
 from django.http import HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 import requests
 import json
+from .function import handle_uploaded_file
+
 
 
 
@@ -13,14 +15,22 @@ def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            myfile = request.FILES['file']
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            return render(request, 'upload.html', {
-                'form': form,
-                'uploaded_file_url': uploaded_file_url
-            })
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse('Successfully uploaded')
+
+        
+            
     else:
         form = UploadFileForm()
+
     return render(request, 'index.html', {'form': form})
+
+
+def uploaded_file_handler(uploaded_file_url):
+    files = {
+        'file': (open('uploaded_file_url', 'rb'))
+    }
+
+
+    response = requests.post('https://yaraify-api.abuse.ch/api/v1/', files=files, verify=True)
+    print(response.text)
